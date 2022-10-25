111be2260fdf6518e5e4bda841161554d95af682
LaMeraChuleta <geffreyytorres25@gmail.com>
2022-10-25 12:27:16 -0500

    pull gatewayInmobiliaria

--- ServerBackEnd/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ServerBackEnd/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,21 +81,23 @@ namespace ApiGateway.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [DataType(DataType.Text)]
             [Display(Name = "Nombres")]
-            public string Nombres { get; set; }
+            public string Name { get; set; }
 
             [Required]
+            [DataType(DataType.Text)]
             [Display(Name = "Apellidos")]
-            public string Apelldios { get; set; }
-            //    /// <summary>
-            //    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            //    ///     directly from your code. This API may change or be removed in future releases.
-            //    /// </summary>
-            //    [Required]
-            //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            //    [DataType(DataType.Password)]
-            //    [Display(Name = "Password")]
-            //    public string Password { get; set; }
+            public string LastName { get; set; }
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            //[Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
 
             //    /// <summary>
             //    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -121,10 +123,11 @@ namespace ApiGateway.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-                const string _passwordFake = "BackMonos";
+                user.Name = Input.Name;
+                user.LastName = Input.LastName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, _passwordFake);
+                var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded) result = await _userManager.AddToRoleAsync(user, "User");
 
                 if (result.Succeeded)
