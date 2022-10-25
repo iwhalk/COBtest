f8764a25d0dfaa93b6c8e85ee40f7245e97dbe97
LaMeraChuleta <geffreyytorres25@gmail.com>
2022-10-25 12:17:27 -0500

    change registros

--- ServerBackEnd/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ServerBackEnd/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,24 +80,31 @@ namespace ApiGateway.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
+            [Display(Name = "Nombres")]
+            public string Nombres { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+            [Required]
+            [Display(Name = "Apellidos")]
+            public string Apelldios { get; set; }
+            //    /// <summary>
+            //    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            //    ///     directly from your code. This API may change or be removed in future releases.
+            //    /// </summary>
+            //    [Required]
+            //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            //    [DataType(DataType.Password)]
+            //    [Display(Name = "Password")]
+            //    public string Password { get; set; }
+
+            //    /// <summary>
+            //    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            //    ///     directly from your code. This API may change or be removed in future releases.
+            //    /// </summary>
+            //    [DataType(DataType.Password)]
+            //    [Display(Name = "Confirm password")]
+            //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            //    public string ConfirmPassword { get; set; }
         }
 
 
@@ -114,10 +121,10 @@ namespace ApiGateway.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                const string _passwordFake = "BackMonos";
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
+                var result = await _userManager.CreateAsync(user, _passwordFake);
                 if (result.Succeeded) result = await _userManager.AddToRoleAsync(user, "User");
 
                 if (result.Succeeded)
