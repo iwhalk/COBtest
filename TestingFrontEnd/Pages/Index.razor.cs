b57405239aa7386416802c3cfe74948d114b2113
LaMeraChuleta <geffreyytorres25@gmail.com>
2022-10-31 14:15:42 -0600

    ARI-92-Again

--- TestingFrontEnd/Pages/Index.razor.cs
+++ TestingFrontEnd/Pages/Index.razor.cs
@@ -4,13 +4,6 @@ using System.Reflection.Metadata.Ecma335;
 namespace TestingFrontEnd.Pages
 {
     public partial class Index : ComponentBase
-    {
-        public bool OpenModal { get; set; } = false;
-        public string TypeIndex { get; set; } = "/";
-        public void ChangeOpenModal() => OpenModal = OpenModal ? false : true;
-        public void ChangeTypeIndex(string newTypeIndex)
-        {
-            TypeIndex = newTypeIndex;
-        }
+    { 
     }
 }
--- TestingFrontEnd/Pages/IndexReceptionCertificates.razor.cs
+++ TestingFrontEnd/Pages/IndexReceptionCertificates.razor.cs
@@ -4,5 +4,12 @@ namespace TestingFrontEnd.Pages
 {
     public partial class IndexReceptionCertificates : ComponentBase
     {
+        public bool OpenModal { get; set; } = false;
+        public string TypeIndex { get; set; } = "/";
+        public void ChangeOpenModal() => OpenModal = OpenModal ? false : true;
+        public void ChangeTypeIndex(string newTypeIndex)
+        {
+            TypeIndex = newTypeIndex;
+        }
     }
 }
