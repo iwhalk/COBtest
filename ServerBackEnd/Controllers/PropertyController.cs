fd5c9a9c80be8ccb7c049479a61f8aef72f5cc48
unknown <dzwk19@outlook.com>
2022-10-31 10:27:00 -0600

    .

--- ServerBackEnd/Controllers/PropertyController.cs
+++ ServerBackEnd/Controllers/PropertyController.cs
@@ -1,15 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiGateway.Interfaces;
+using ApiGateway.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
 
 namespace ApiGateway.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class PropertyController : ControllerBase
     {
-        public PropertyController()
+        private readonly IPropertyService _propertyService;
+        public PropertyController(IPropertyService propertyService)
         {
+            _propertyService = propertyService;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult> GetProperty()
+        {
+            var result = await _propertyService.GetPropertyAsync();
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostProperty(Property property)
+        {
+            var result = await _propertyService.PostPropertyAsync(property);
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
         }
     }
 }
--- ServerBackEnd/Controllers/TenantController.cs
+++ ServerBackEnd/Controllers/TenantController.cs
@@ -1,15 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiGateway.Interfaces;
+using ApiGateway.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
 
 namespace ApiGateway.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class TenantController : ControllerBase
     {
-        public TenantController()
+        private readonly ITenantService _tenantService;
+        public TenantController(ITenantService tenantService)
         {
+            _tenantService = tenantService;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult> GetTenant()
+        {
+            var result = await _tenantService.GetTenantAsync();
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostTenant(Tenant tenant)
+        {
+            var result = await _tenantService.PostTenantAsync(tenant);
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
         }
     }
 }
