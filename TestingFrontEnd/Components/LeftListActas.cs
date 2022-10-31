e69ea551eb577c756048211775bca1066aec7724
Eliasim Mendoza Salgado <90936660+Eliasim@users.noreply.github.com>
2022-10-31 12:36:24 -0600

    Componente LeftListActas

---
+++ TestingFrontEnd/Components/LeftListActas.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Shared.Components
+{
+    public partial class LeftListActas : ComponentBase
+    {
+        public LeftListActas()
+        {
+
+        }
+    }
+}
