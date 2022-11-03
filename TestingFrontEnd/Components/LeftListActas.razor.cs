f5687013d8dfd5eb22c736a5ad7c7aea6e01b19a
LaMeraChuleta <geoffreyytorres25@gmail.com>
2022-11-03 11:01:11 -0600

    Add Modal Iventory initial

---
+++ TestingFrontEnd/Components/LeftListActas.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestingFrontEnd.Components
+{
+    public partial class LeftListActas : ComponentBase
+    {
+        [Parameter]
+        public string TypeList { get; set; } = "";
+        [Parameter]
+        public EventCallback OnClick { get; set; }
+    }
+}
---
+++ TestingFrontEnd/Pages/InventoryReceptionCertificates.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestingFrontEnd.Pages
+{
+    public partial class InventoryReceptionCertificates : ComponentBase
+    {
+        public bool ShowModalRooms { get; set; } = false;
+        public void ChangeOpenModalRooms() => ShowModalRooms = ShowModalRooms ? false : true;
+        public bool ShowModalComponents { get; set; } = false;
+        public void ChangeOpenModalComponents() => ShowModalComponents = ShowModalComponents ? false : true;
+    }
+}
