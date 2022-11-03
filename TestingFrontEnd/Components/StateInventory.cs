d572e10be83e55d457bbc815313bcb350d810ffc
EliasimMS <111379457+EliasimMS@users.noreply.github.com>
2022-11-03 12:38:50 -0600

    Vista Emails y Signatures

---
+++ TestingFrontEnd/Components/StateInventory.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Shared.Components
+{
+    public partial class StateInventory : ComponentBase
+    {
+        public StateInventory()
+        {
+
+        }
+    }
+}
