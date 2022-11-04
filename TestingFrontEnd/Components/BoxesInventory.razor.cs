cf0b58fc292234ef86a82b45da71c9865daeb442
LaMeraChuleta <geffreyytorres25@gmail.com>
2022-11-04 12:54:23 -0600

    Add Plus in image compoennte

--- TestingFrontEnd/Components/BoxesInventory.razor.cs
+++ TestingFrontEnd/Components/BoxesInventory.razor.cs
@@ -4,6 +4,7 @@ namespace Shared.Components
 {
     public partial class BoxesInventory : ComponentBase
     {
+        public int MyProperty { get; set; }
         public BoxesInventory()
         {
 
