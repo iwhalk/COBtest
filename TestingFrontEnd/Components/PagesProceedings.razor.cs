0a63f331fd4a0f2246eac52a9def92617e86b694
Eliasim Mendoza Salgado <90936660+Eliasim@users.noreply.github.com>
2022-11-07 10:32:08 -0600

    Iconos login cambios

---
+++ TestingFrontEnd/Components/PagesProceedings.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Shared.Components
+{
+    public partial class PagesProceedings : ComponentBase
+    {
+        public PagesProceedings()
+        {
+
+        }
+    }
+}
