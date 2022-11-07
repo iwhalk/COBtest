95d1e3d90a0816bca6da4a4019b26db9771a3407
LaMeraChuleta <geffreyytorres25@gmail.com>
2022-11-07 15:41:17 -0600

    Rama emi anbiguedad

---
+++ TestingFrontEnd/Components/Lessors/Form.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared.Models;
+namespace TestingFrontEnd.Components.Lessors
+{
+    public partial class Form : ComponentBase
+    {
+        [Parameter]
+        public EventCallback OpenModalLessor { get; set; }
+        [Parameter]
+        public Lessor? lessor { get; set; }
+
+    }
+}
