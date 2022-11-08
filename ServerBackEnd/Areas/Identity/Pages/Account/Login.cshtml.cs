f21d15870d466ec2c729fff5ff9651784484b59b
Eliasim Mendoza Salgado <90936660+Eliasim@users.noreply.github.com>
2022-11-08 12:19:08 -0600

    boton para mostrar contraseña inicializado

--- ServerBackEnd/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ServerBackEnd/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -141,5 +141,7 @@ namespace ApiGateway.Areas.Identity.Pages.Account
             // If we got this far, something failed, redisplay form
             return Page();
         }
+
+        
     }
 }
