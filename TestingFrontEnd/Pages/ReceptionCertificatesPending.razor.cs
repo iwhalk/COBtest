3fa3c17e247e333d72da8d666e0f4e62fac852df
unknown <dzwk19@outlook.com>
2022-11-09 13:58:20 -0600

    Se agregaron las clases parciales de Pending y Historical, inyectando en cada una de ellas los 9 servicios para su respectivo consumo de apis

---
+++ TestingFrontEnd/Pages/ReceptionCertificatesPending.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared.Models;
+using TestingFrontEnd.Interfaces;
+using TestingFrontEnd.Stores;
+
+namespace TestingFrontEnd.Pages
+{
+    public partial class ReceptionCertificatesPending : ComponentBase
+    {
+        private readonly ApplicationContext _context;
+        private readonly ITenantService _tenantService;
+        private readonly ILessorService _lessorService;
+        private readonly IPropertyService _propertyService;
+        private readonly IInventoryService _inventoryService;
+        private readonly IServicesService _servicesService;
+        private readonly IAreaService _areaService;
+        private readonly IDescriptionService _descriptionService;
+        private readonly IFeaturesService _featuresService;
+        private readonly IPropertyTypeService _propertyTypeService;
+
+        public ReceptionCertificatesPending(ApplicationContext context, ITenantService tenantService, IPropertyService propertyService, ILessorService lessorService, IInventoryService inventoryService, IServicesService servicesService, IAreaService areaService, IDescriptionService descriptionService, IFeaturesService featuresService, IPropertyTypeService propertyTypeService)
+        {
+            _context = context;
+            _tenantService = tenantService;
+            _propertyService = propertyService;
+            _lessorService = lessorService;
+            _inventoryService = inventoryService;
+            _servicesService = servicesService;
+            _areaService = areaService;
+            _descriptionService = descriptionService;
+            _featuresService = featuresService;
+            _propertyTypeService = propertyTypeService;
+        }
+
+        private List<Tenant> tenants { get; set; }
+        private List<Lessor> lessors { get; set; }
+        private List<Property> properties { get; set; }
+        private List<Inventory> inventories { get; set; }
+        private List<Service> services { get; set; }
+        private List<Area> areas { get; set; }
+        private List<Description> descriptions { get; set; }
+        private List<Feature> features { get; set; }
+        private List<PropertyType> propertyTypes { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            tenants = await _tenantService.GetTenantAsync();
+            lessors = await _lessorService.GetLessorAsync();
+            properties = await _propertyService.GetPropertyAsync();
+            inventories = await _inventoryService.GetInventoryAsync();
+            services = await _servicesService.GetServicesAsync();
+            areas = await _areaService.GetAreaAsync();
+            descriptions = await _descriptionService.GetDescriptionAsync();
+            features = await _featuresService.GetFeaturesAsync();
+            propertyTypes = await _propertyTypeService.GetPropertyTypeAsync();
+        }
+    }
+}
