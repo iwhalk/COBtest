fa1c82c66d62b5bfdb0619fd40f5b70b93851567
LaMeraChuleta <geffreyytorres25@gmail.com>
2022-11-09 11:21:52 -0600

    bind-Value

--- TestingFrontEnd/Pages/CreateReceptionCertificates.razor.cs
+++ TestingFrontEnd/Pages/CreateReceptionCertificates.razor.cs
@@ -8,7 +8,7 @@ namespace TestingFrontEnd.Pages
         public bool ShowModalLessor { get; set; } = false;
         public bool ShowModalTenant { get; set; } = false;
         public bool ShowModalProperty { get; set; } = false;
-        public Lessor? NewLessor { get; set; } = new Lessor();
+        public Lessor? NewLessor { get; set; } = new Lessor { Name= "Luis", LastName = "EMiliano", EmailAddress = "luis@gmail.com", Cp = "14100", Colony = "Pedregal de Sn Nicolas", Delegation = "Tlalpan", PhoneNumber = "5556441051", Rfc = "3234235234", Street = "Sisal"};
 
         public void ChangeOpenModalLessor() => ShowModalLessor = ShowModalLessor ? false : true;
         public void ChangeOpenModalTenant() => ShowModalTenant = ShowModalTenant ? false : true;
