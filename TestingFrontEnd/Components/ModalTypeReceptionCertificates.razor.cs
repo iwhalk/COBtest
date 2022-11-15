b625c7197e3f2d82c03cef8bd1bd00ea3c0c1015
LaMeraChuleta <geoffreyytorres25@gmail.com>
2022-11-15 10:12:49 -0600

    _context Reception in out

--- TestingFrontEnd/Components/ModalTypeReceptionCertificates.razor.cs
+++ TestingFrontEnd/Components/ModalTypeReceptionCertificates.razor.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FrontEnd.Stores;
+using Microsoft.AspNetCore.Components;
+using TanvirArjel.Blazor.Extensions;
 
 namespace FrontEnd.Components
 {
     public partial class ModalTypeReceptionCertificates : ComponentBase
     {
+        private readonly NavigationManager _navigationManager;
+        private readonly ApplicationContext _context;
+
         [Parameter]
         public bool ShowModal { get; set; }
         [Parameter]
         public EventCallback OnClick { get; set; }
+
+        public ModalTypeReceptionCertificates(ApplicationContext context, NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+            _context = context;
+        }
+
+        public void RedirectReceptionIn()
+        {
+            _context.TypeReceptionCertificate = 1;
+            _navigationManager.NavigateTo("/ReceptionCertificates/Create");
+        }
+        public void RedirectReceptionOut()
+        {
+            _context.TypeReceptionCertificate = 2;
+            _navigationManager.NavigateTo("/ReceptionCertificates/Create");
+        }
     }
 }
