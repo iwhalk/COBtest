109cedb20854c6b2d730ea3c712e6aa8bfb0198f
unknown <dzwk19@outlook.com>
2022-11-18 13:47:55 -0600

    Tabla de habitaciones llena

--- TestingFrontEnd/Components/ModalRoomsOrComponents.razor.cs
+++ TestingFrontEnd/Components/ModalRoomsOrComponents.razor.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using SharedLibrary.Models;
 
 namespace FrontEnd.Components
 {
     public partial class ModalRoomsOrComponents : ComponentBase
     {
+        [Parameter]
+        public List<Area> Areas { get; set; }
         [Parameter]
         public string TitleTable { get; set; } = "";
         [Parameter]
